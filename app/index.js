0ffa02e5ba8d01665b7606759aeed62a4c58cc8f
Ashutosh Mishra <ashutoshdtu@gmail.com>
2023-09-06 10:23:20 +0530

    Use BASE_PATH as the path to mount graphql explorer on

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {altairExpress} = require('@appwrite.io/altair-express-middleware');
+const { altairExpress } = require('@appwrite.io/altair-express-middleware');
 
 const app = express();
 
-// Mount your altair GraphQL client
-app.use('/', altairExpress({
+// Get BASE_PATH from environment variables or default to '/'
+const basePath = process.env.BASE_PATH || '/';
+
+// Mount your Altair GraphQL client
+app.use(basePath, altairExpress({
     endpointURL: process.env.SERVER_URL,
     initialHeaders: {
         "X-Appwrite-Project": "",
@@ -27,5 +30,6 @@ app.use('/', altairExpress({
 
 app.listen(3000, () => {
     console.log("GraphQL Explorer listening on port 3000!");
-    console.log("Endpoint url: " + process.env.SERVER_URL)
+    console.log("Endpoint url: " + process.env.SERVER_URL);
+    console.log("Base path: " + basePath);
 });
